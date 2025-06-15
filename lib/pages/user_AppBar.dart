450509d7c295e4ac676afe797ccbcf40b7ed87be
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-06-15 16:28:49 +0800

    Add User Appbar scaffold

---
+++ lib/pages/user_AppBar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserAppbar extends StatelessWidget {
+  const UserAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
