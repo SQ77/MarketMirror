b095031a7129be4e04753710561b3c7480b4e56e
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-06-15 20:59:32 +0800

    Move files into their respective folders

---
+++ lib/pages/news/trending_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TrendingPage extends StatelessWidget {
+  const TrendingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(
+        child: Text("Trending Page")));
+  }
+}
