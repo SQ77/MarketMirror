35f0489a41321fa79cb8d84916a338af0716f4a8
SQ77 <andrewzsq@gmail.com>
2025-06-18 21:24:00 +0800

    Fix flutter warnings

--- lib/pages/userAppBar/user_AppBar.dart
+++ lib/pages/userAppBar/user_AppBar.dart
@@ -4,24 +4,19 @@ import 'package:marketmirror/pages/userAppBar/user_Profile.dart';
 class UserAppbar extends StatelessWidget implements PreferredSizeWidget {
   const UserAppbar({super.key});
 
-  void _checkProfile() {
-    // Handle profile logic here
-  }
-
   @override
   Widget build(BuildContext context) {
     return AppBar(
       leading: Builder(
-        builder: (context) => IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: () => Scaffold.of(context).openDrawer(),
-        ),
+        builder:
+            (context) => IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () => Scaffold.of(context).openDrawer(),
+            ),
       ),
       backgroundColor: const Color.fromARGB(255, 212, 181, 212),
-      title: const Text("MARKET MIRROR"),
-      actions: [
-        const UserProfile()
-      ],
+      title: const Text("Market Mirror"),
+      actions: [const UserProfile()],
     );
   }
 
