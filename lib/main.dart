0dd53e375d7d34578cdd246bea1f40cf2238f5ea
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-06-18 20:25:32 +0800

    Fix error

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ Future main() async {
   }
 
   await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
-  runApp();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
