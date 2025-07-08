a8ba0482218e88fc3e1b04d46173c7ea049d1fba
SQ77 <andrewzsq@gmail.com>
2025-07-08 19:55:06 +0800

    Add predict page

---
+++ lib/pages/news/predict_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PredictPage extends StatelessWidget {
+  const PredictPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Predict Page"));
+  }
+}
