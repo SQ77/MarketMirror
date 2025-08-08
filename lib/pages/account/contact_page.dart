d4272a4de5eb24112e3ec536b335e68ec5aef5a0
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-08-08 15:22:54 +0800

    Add basic styling for contact page

--- lib/pages/account/contact_page.dart
+++ lib/pages/account/contact_page.dart
@@ -5,8 +5,39 @@ class ContactPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("Contact Us"),),
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(right: 30, left: 30),
+        child: Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 80),
+              Icon(Icons.person, size: 80),
+              SizedBox(height: 40),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                ),
+              ),
+              SizedBox(height: 40),
+              Text("Change Password"),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Old Password',
+                ),
+              ),
+              SizedBox(height: 30),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'New Password',
+                ),
+              )
+            ],
+          ) 
+        ),
+      )
     );
   }
 }
