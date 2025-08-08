bf5122c021b18c0ed73d88b22d536a94e2f7d373
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-08-08 16:15:43 +0800

    Add form fields

--- lib/pages/account/contact_page.dart
+++ lib/pages/account/contact_page.dart
@@ -8,9 +8,30 @@ class ContactPage extends StatelessWidget {
     return Center(
       child: Padding(
         padding : const EdgeInsets.only(right: 30, left: 30),
-        child: Column(children: [
-          
-        ],)
+        child: Form(
+          child: Column(
+            children: [
+              SizedBox(height: 200),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Subject'),
+              ),
+              SizedBox(height: 40),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Body'),
+              ),
+              SizedBox(height: 40),
+              OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                child: Text("Submit Query"),
+              ),
+            ]
+          ) 
+        )
       )
     );
   }
